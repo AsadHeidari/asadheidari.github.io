25d1818454e96a3561e2c95b6ca30c108884859e
asadHeidari <asadheidari86@gmail.com>
2024-11-26 23:31:26 +0330

    Remove link around logo image in Header component.

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -8,9 +8,7 @@ function Header(): JSX.Element {
   return (
     <div className="fixed left-0 top-0 w-full flex flex-row items-center justify-between text-sm font-vazir backdrop-blur-sm z-10 px-4">
       <nav className=" flex justify-center items-center pb-6 pt-6 lg:static lg:w-auto gap-2 "></nav>
-      <a href="https://asadheidari.ir/" target="_blank">
-        <img src="/images/logo.png" alt="logo" width={100} height={24} />
-      </a>
+      <img src="/images/logo.png" alt="logo" width={100} height={24} />
     </div>
   );
 }
